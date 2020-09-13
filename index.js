ee39860f6568fe71acb0989603c0f11c83521a42
iammadab <iammadab@gmail.com>
2020-09-13 16:20:29 +0100

    Version 1.0

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const makeExtractMarkets = require("./extractMarkets")
 const extractMarkets = makeExtractMarkets()
 
-const markets = extractMarkets("p1", { a: 3 })
-console.log(markets)
+module.exports = extractMarkets
