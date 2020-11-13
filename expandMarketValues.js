3dcfa4f0b26e52d8a545d2139c5925b384813c3e
iammadab <iammadab@gmail.com>
2020-11-13 16:10:41 +0100

    Integrated user friendly naming with market generation

--- expandMarketValues.js
+++ expandMarketValues.js
@@ -1,3 +1,8 @@
+// Converts from 1X2:1 to { 1X2: { 1: val } }
+// Also converts the group and outcome to user friendly names
+
+const nameMap = require("./name")
+
 const expandMarketValues = compressedMarket => {
 	
 	const expandedValues = {}
@@ -8,6 +13,9 @@ const expandMarketValues = compressedMarket => {
 
 		if(!expandedValues[marketGroup])
 			expandedValues[marketGroup] = {}
+
+		marketGroup = nameMap[marketGroup] || marketGroup
+		marketOutcome = nameMap[marketOutcome] || marketOutcome
 
 		expandedValues[marketGroup][marketOutcome] = compressedMarket[key]
 
